dcb9ea837358c37faff02e99820c36511d718594
arya-khanna <aryakhanna@hotmail.com>
2020-01-12 08:29:12 +0000

    solved migrationg issue by dropping empty users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_070823) do
+ActiveRecord::Schema.define(version: 2020_01_12_082051) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
