c36605028fc32d63c318712eb90ce57cea8b704b
arya-khanna <aryakhanna@hotmail.com>
2020-01-19 13:25:14 +0000

    implement basic login and signout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,26 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
   
+  def setup
+    @user = users(:michael)
+  end
+  
+  # 1. Visit the login path.
+  # 2. Post valid information to the sessions path.
+  # 3. Verify that the login link disappears.
+  # 4. Verify that a logout link appears
+  # 5. Verify that a profile link appears.
+  test "login with valid information" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'foobar' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+  
   # 1. Visit the login path.
   # 2. Verify that the new sessions form renders properly.
   # 3. Post to the sessions path with an invalid params hash.
@@ -21,5 +41,6 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end 
+  
   
 end
