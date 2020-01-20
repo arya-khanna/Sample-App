aa51f63f7c1d4cc5b64e23a399c156444717f559
arya-khanna <aryakhanna@hotmail.com>
2020-01-20 01:07:17 +0000

    added edit form with unsuccessful edit test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,26 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
+  end 
+  
+  def edit
+    @user = User.find(params[:id])
+  end 
+  
+  def update
+    @user = User.find(params[:id])
+    # Handle a successful update
+    if @user.update(user_params)
+      #handle success
+    else
+      render 'edit'
+    end 
   end 
   
   private 
