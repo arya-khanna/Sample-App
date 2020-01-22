acbf92af76d2b08dc98023eaa5e1b5a5a7e5dcba
arya-khanna <aryakhanna@hotmail.com>
2020-01-22 03:55:58 +0000

    users will be sent an activation email upon sign-up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
+  def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end 
@@ -17,7 +17,6 @@ class UserMailer < ApplicationMailer
   #
   def password_reset
     @greeting = "Hi"
-
     mail to: "to@example.org"
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  host = 'https://072d250afbe542d2ac975a8fdb8cf084.vfs.cloud9.us-east-2.amazonaws.com/'
+  
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
